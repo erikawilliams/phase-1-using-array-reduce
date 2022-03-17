522eae41acdbe3497cb5f0fb17541f8fd686d715
Erika Williams <erika@erikawilliams.us>
2022-03-17 15:27:00 -0600

    commit tree 989d66f119b54ece02323c1e2ff8ea8439f124cd

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = function(array){
+//     const total = array.reduce(function(accum,element){return accum + element},0)
+//     console.log(total)
+//     return total
+// }
+
+const totalBatteries = batteryBatches.reduce((accum,element)=>(accum+element),0)
+console.log(totalBatteries)
